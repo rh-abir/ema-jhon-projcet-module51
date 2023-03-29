c9f97b760662124fa1989dc442b2072d274f7ce6
rh-abir <rhabir.co@gmail.com>
2023-03-29 17:34:01 +0600

    added cart product and calulate

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         fetch('products.json')
@@ -30,8 +31,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h4>Order Summary</h4>
-                <p>Selected Items: {cart.length}</p>
+                <Cart cart = {cart}></Cart>
             </div>
         </div>
     );
