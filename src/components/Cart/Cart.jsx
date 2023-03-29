d0c73c0c759510ad0de38ed0038b4e424d686897
rh-abir <rhabir.co@gmail.com>
2023-03-29 20:34:23 +0600

    save card localStorage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import './Cart.css';
 
 const Cart = ({cart}) => {
-console.log(cart)
-let totalPrice = 0;
-let totalShipping = 0;
-for(const product of cart){
-    totalPrice = totalPrice + product.price;
-    totalShipping = totalShipping + product.shipping;
-}
+    console.log(cart)
+    let totalPrice = 0;
+    let totalShipping = 0;
+    for(const product of cart){
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
+    }
 let tax = totalPrice * 7 / 100;
 const grandTotal  = totalPrice + totalShipping + tax;
   return (
