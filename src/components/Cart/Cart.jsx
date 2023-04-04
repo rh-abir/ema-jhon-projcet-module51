c9aeeb3038370ab3a826e9b49e7ea0208d4ec9f6
rh-abir <rhabir.co@gmail.com>
2023-04-04 20:08:07 +0600

    restart file

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,21 +1,31 @@
-import React from "react";
-import './Cart.css';
+import React, { PureComponent } from "react";
+import "./Cart.css";
 
-const Cart = ({cart}) => {
-    console.log(cart)
-    let totalPrice = 0;
-    let totalShipping = 0;
-    for(const product of cart){
-        totalPrice = totalPrice + product.price;
-        totalShipping = totalShipping + product.shipping;
-    }
-let tax = totalPrice * 7 / 100;
-const grandTotal  = totalPrice + totalShipping + tax;
+const Cart = ({ cart }) => {
+
+  console.log(cart);
+  let totalPrice = 0;
+  let totalShipping = 0;
+  let quantity = 0;
+  for (const product of cart) {
+    
+    
+    // if(product.quantity === 0){
+    //     product.quantity = 1;
+    // }
+
+    // product.quantity = product.quantity || 1;
+    totalPrice = totalPrice + product.price * product.quantity;
+    totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
+  }
+  let tax = (totalPrice * 7) / 100;
+  const grandTotal = totalPrice + totalShipping + tax;
   return (
     <div className="cart">
       <h4>Order Summary</h4>
-      <p>Selected Items: {cart.length}</p>
-    <p>Total Price: ${totalPrice}</p>
+      <p>Selected Items: {quantity}</p>
+      <p>Total Price: ${totalPrice}</p>
       <p>Shipping: ${totalShipping}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
       <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
